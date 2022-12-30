01dde2e064f58b4309e8ddbf974338c5c977166e
SeoJiHyeon <sgh9702@naver.com>
2022-12-30 10:22:16 +0900

    이미지 업로드(각각의 client 마다 폴더 생성)

--- src/main/java/aws/s3/profile/UserProfileService.java
+++ src/main/java/aws/s3/profile/UserProfileService.java
@@ -1,20 +1,26 @@
 package aws.s3.profile;
 
+import aws.s3.bucket.BucketName;
+import aws.s3.filestore.FileStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
-import java.util.UUID;
+import java.io.IOException;
+import java.util.*;
+
+import static org.apache.http.entity.ContentType.*;
 
 @Service
 public class UserProfileService {
 
     private final UserProfileDataAccessService userProfileDataAccessService;
+    private final FileStore fileStore;
 
     @Autowired
-    public UserProfileService(UserProfileDataAccessService userProfileDataAccessService) {
+    public UserProfileService(UserProfileDataAccessService userProfileDataAccessService, FileStore fileStore) {
         this.userProfileDataAccessService = userProfileDataAccessService;
+        this.fileStore = fileStore;
     }
 
     List<UserProfile> getUserProfiles(){
@@ -22,6 +28,50 @@ public class UserProfileService {
     }
 
     public void uploadUserProfileImage(UUID userProfileId, MultipartFile file) {
+        ifFileEmpty(file);
 
+        ifImage(file);
+
+        UserProfile user = getUserProfileOrThrow(userProfileId);
+
+        Map<String, String> metadata = extractMetadata(file);
+
+        String path = String.format("%s/%s", BucketName.PROFILE_IMAGE.getBucketName(), user.getUserProfileId());
+        String filename = String.format("%s-%s", file.getOriginalFilename(), UUID.randomUUID());
+
+        try {
+            fileStore.save(path, filename, Optional.of(metadata), file.getInputStream());
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    private Map<String, String> extractMetadata(MultipartFile file) {
+        Map<String, String> metadata = new HashMap<>();
+        metadata.put("Content-Type", file.getContentType());
+        metadata.put("Content-Length", String.valueOf(file.getSize()));
+        return metadata;
+    }
+
+    private UserProfile getUserProfileOrThrow(UUID userProfileId) {
+        return userProfileDataAccessService
+                .getUserProfiles()
+                .stream()
+                .filter(userProfile -> userProfile.getUserProfileId().equals(userProfileId))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("user profile 을 찾을수 없습니다."));
+    }
+
+    private void ifImage(MultipartFile file) {
+        if(!Arrays.asList(IMAGE_JPEG.getMimeType(), IMAGE_GIF.getMimeType(), IMAGE_PNG.getMimeType())
+                .contains(file.getContentType())){
+            throw new IllegalStateException("이미지 파일이여야 합니다.");
+        }
+    }
+
+    private void ifFileEmpty(MultipartFile file) {
+        if (file.isEmpty()) {
+            throw new IllegalStateException("파일이 없습니다.");
+        }
     }
 }
